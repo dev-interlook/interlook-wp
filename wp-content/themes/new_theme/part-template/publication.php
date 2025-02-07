acb01950386d452a8571a86656a7ded4fea84714
Taufiq Ridwan <taufiq26.ridwan@gmail.com>
2025-02-07 23:46:54 +0700

    add preview video if the related url of publication is youtube link and made the image video's youtube thumbnail

--- wp-content/themes/new_theme/part-template/publication.php
+++ wp-content/themes/new_theme/part-template/publication.php
@@ -70,7 +70,35 @@ get_header();
         color: rgba(51,51,51,.7);
         margin: 0px 0px 15px 0px;
     }
+
+    .youtube-preview { position: relative; display: block; }
+    .youtube-preview-overlay {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0,0,0,0.7);
+        opacity: 0;
+        transition: opacity 0.3s;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        z-index: 1;
+    }
+    .youtube-preview:hover .youtube-preview-overlay { opacity: 0; }
+    .youtube-preview iframe {
+        z-index: 2;
+    }
 </style>
+
+<?php
+    function get_youtube_id_from_url($url) {
+        $pattern = '/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/i';
+        preg_match($pattern, $url, $matches);
+        return isset($matches[1]) ? $matches[1] : false;
+    }
+?>
 
 <!-- Content -->
 <div class="content-wrapper">
@@ -84,15 +112,20 @@ get_header();
                 <!-- List of publications -->
                 <div>
                     <?php if ( have_rows( 'publications' ) ) : ?>
-                        <?php while ( have_rows( 'publications' ) ) : the_row(); ?>
+                        <?php while ( have_rows( 'publications' ) ) : the_row(); 
+                            $related_url = get_sub_field('related_url');
+                            $youtube_id = get_youtube_id_from_url($related_url);
+                        ?>
                             <figure>
-                                <a href="<?php the_sub_field( 'related_url' ); ?>" target="_blank">
-                                    <?php if ( get_sub_field( 'image' ) ) { ?>
-                                        <img src="<?php the_sub_field( 'image' ); ?>" alt="A windmill" />
-                                    <?php } ?>
+                                <a href="<?php echo esc_url($related_url); ?>" target="_blank" <?php echo $youtube_id ? 'class="youtube-preview" data-video-id="' . esc_attr($youtube_id) . '"' : ''; ?>>
+                                    <?php if ($youtube_id): ?>
+                                        <img src="https://img.youtube.com/vi/<?php echo esc_attr($youtube_id); ?>/maxresdefault.jpg" alt="<?php the_sub_field('title'); ?>" />
+                                    <?php elseif (get_sub_field('image')): ?>
+                                        <img src="<?php the_sub_field('image'); ?>" alt="<?php the_sub_field('title'); ?>" />
+                                    <?php endif; ?>
                                     <figcaption>
-                                        <h6><?php the_sub_field( 'title' ); ?></h6>
-                                        <p><?php the_sub_field( 'sub_title' ); ?></p>
+                                        <h6><?php the_sub_field('title'); ?></h6>
+                                        <p><?php the_sub_field('sub_title'); ?></p>
                                     </figcaption>
                                 </a>
                             </figure>
@@ -106,3 +139,39 @@ get_header();
     </section>
 
 <?php get_footer();?>
+
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    const previews = document.querySelectorAll('.youtube-preview');
+    
+    previews.forEach(preview => {
+        let previewTimer;
+        
+        preview.addEventListener('mouseenter', function(e) {
+            const videoId = this.getAttribute('data-video-id');
+            
+            previewTimer = setTimeout(() => {
+                const iframe = document.createElement('iframe');
+                iframe.setAttribute('src', `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`);
+                iframe.style.position = 'absolute';
+                iframe.style.top = '0';
+                iframe.style.left = '0';
+                iframe.style.width = '100%';
+                iframe.style.height = '100%';
+                iframe.style.border = 'none';
+                iframe.style.pointerEvents = 'none'; // This prevents iframe from capturing clicks
+                iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
+                
+                this.style.position = 'relative';
+                this.insertBefore(iframe, this.firstChild);
+            }, 1000);
+        });
+        
+        preview.addEventListener('mouseleave', function() {
+            clearTimeout(previewTimer);
+            const iframe = this.querySelector('iframe');
+            if (iframe) iframe.remove();
+        });
+    });
+});
+</script>
